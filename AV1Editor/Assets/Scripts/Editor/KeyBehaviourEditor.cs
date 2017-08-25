7c362cb0beee23fce74e8b420523d5d7534d2dd3
Juliana Potengy <julianalpotengy@gmail.com>
2017-08-25 08:52:52 -0300

    Custom editor
    Fazer custom editor de um MonoBehaviour que contém uma List<Entry> +
    extras

---
+++ AV1Editor/Assets/Scripts/Editor/KeyBehaviourEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(KeyBehaviour))]
+public class KeyBehaviourEditor : Editor
+{
+    //fazer editor de um monobehaviour que contem uma tabela. cada linha deve conter a chave e o valor lado a lado
+    //essa tabela é um List<Entry> Entry { string key; int value; }
+    //extras: fazer botao de adicionar elemento e fazer botao de remover elemento 
+
+    int remove;
+
+    public override void OnInspectorGUI()
+    {
+        KeyBehaviour keyBehaviour = target as KeyBehaviour;
+        remove = -1;
+
+        EditorGUILayout.LabelField("List", EditorStyles.whiteLargeLabel);
+
+        foreach (KeyBehaviour.Entry i in keyBehaviour.listEntry)
+        {
+            EditorGUILayout.BeginHorizontal();
+            i.key = EditorGUILayout.TextField(i.key);
+            i.value = EditorGUILayout.IntField(i.value);
+            if (GUILayout.Button("remove"))
+            {
+                remove = keyBehaviour.listEntry.IndexOf(i);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if(GUILayout.Button("ADD"))
+        {
+            keyBehaviour.listEntry.Add(new KeyBehaviour.Entry());
+        }
+        if(remove >= 0)
+        {
+            keyBehaviour.listEntry.RemoveAt(remove);
+        }
+    }
+}
---
+++ AV1Editor/Assets/Scripts/KeyBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBehaviour : MonoBehaviour
+{
+    [System.Serializable]
+    public class Entry
+    {
+        public string key = "Key";
+        public int value;
+    }
+
+    public List<Entry> listEntry = new List<Entry>();
+}
